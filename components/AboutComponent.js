860977e802371afb4ad97a5bf49f213cf672548f
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-03-02 01:28:18 -0500

    using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 import { ScrollView, Text, FlatList} from "react-native";
 import { Card, ListItem } from "react-native-elements";
-import { PARTNERS } from "../shared/partners";
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
 
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission(){
     return(
@@ -16,12 +22,7 @@ function Mission(){
 }
 
 class About extends Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            partners: PARTNERS
-        };
-    }
+ 
     static navigationOptions = {
         title: "About Us"
     }
@@ -32,7 +33,7 @@ render(){
             <ListItem
             title= {item.name}
             subtitle={item.description}
-            leftAvatar ={{source:require('./images/bootstrap-logo.png')}}
+            leftAvatar={{source: {uri: baseUrl + item.image}}}
             />
         )
 };
@@ -42,7 +43,7 @@ return (
     <Mission/>
     <Card title="Community Partners">
     <FlatList
-        data={this.state.partners}
+        data={this.props.partners.partners}
         renderItem = {renderPartner}
         keyExtractor = {item => item.id.toString()}
         />
@@ -53,6 +54,7 @@ return (
 }
 
 }
-export default About;
+
+export default connect(mapStateToProps)(About);
 
 
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card, Icon} from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
-import { COMMENTS } from '../shared/comments'; 
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments
+    };
+};
 
 function RenderCampsite(props) {
   const {campsite} = props;
@@ -10,8 +18,8 @@ function RenderCampsite(props) {
         return (
             <Card
                 featuredTitle={campsite.name}
-                image={require('./images/react-lake.jpg')}
-            >
+                image={{uri: baseUrl + campsite.image}}>
+            
                 <Text style={{margin: 10}}>
                     {campsite.description}
                 </Text>
@@ -56,8 +64,6 @@ class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
             favorite: false
         };
     }
@@ -71,8 +77,8 @@ class CampsiteInfo extends Component {
 
     render() {
         const campsiteId = this.props.navigation.getParam('campsiteId');
-        const campsite = this.state.campsites.filter(campsite => campsite.id === campsiteId)[0];
-        const comments = this.state.comments.filter(comment => comment.campsiteId === campsiteId);
+        const campsite = this.props.capmsites.campsites.filter(campsite => campsite.id === campsiteId)[0];
+        const comments = this.props.comments.comments.filter(comment => comment.campsiteId === campsiteId);
         return( 
         <ScrollView>
             <RenderCampsite campsite={campsite} 
@@ -85,4 +91,4 @@ class CampsiteInfo extends Component {
     }
 }
 
-export default CampsiteInfo;
+export default connect(mapStateToProps)(CampsiteInfo);
