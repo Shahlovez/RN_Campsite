3bae5376ed1cabda242a8e2e462d5824feadaac1
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-03-03 00:57:59 -0500

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,6 +4,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image} from 'react-native';
 import {Icon} from 'react-native-elements';
 import Constants from 'expo-constants';
@@ -14,6 +15,7 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
+
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -163,6 +165,20 @@ const MainNavigator =createDrawerNavigator(
             )
         }
     },
+    Reservation: {
+        screen: ReservationNavigator,
+        navigationOptions: {
+            drawerLabel: 'Reserve Campsite',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='tree'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
     About: {
         screen: AboutNavigator,
         navigationOptions: {
@@ -245,4 +261,27 @@ const styles = StyleSheet.create({
         fontSize: 24
     }
 })
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 export default connect(null, mapDispatchToProps)(Main);
