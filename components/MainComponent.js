f08ef0e859eee27c9bf64252e20a56e1aa3fa336
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-03-07 12:23:37 -0500

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -15,6 +15,7 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
+import Favorites from './FavoritesComponent';
 
 
 const mapDispatchToProps = {
@@ -140,6 +141,29 @@ const ReservationNavigator = createStackNavigator(
         })
     }
 );
+
+const FavoritesNavigator = createStackNavigator(
+    {
+        Favorites: { screen: Favorites }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='heart'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -200,6 +224,21 @@ const MainNavigator =createDrawerNavigator(
             )
         }
     },
+    Favorites: {
+        screen: FavoritesNavigator,
+        navigationOptions: {
+            drawerLabel: 'My Favorites',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='heart'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
+    
     About: {
         screen: AboutNavigator,
         navigationOptions: {
