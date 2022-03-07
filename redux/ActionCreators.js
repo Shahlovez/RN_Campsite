efa809226aa95e03eb43d2ae95bdb8df5df3eca8
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-03-07 12:45:45 -0500

    Swipe and Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -167,3 +167,8 @@ export const addFavorite = campsiteId => ({
     type: ActionTypes.ADD_FAVORITE,
     payload: campsiteId
 });
+
+export const deleteFavorite = campsiteId => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: campsiteId
+}); 
