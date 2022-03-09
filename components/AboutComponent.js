1f6ed33a39739411c4828d9489fa3947bf0fd54e
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-03-08 23:00:04 -0500

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from "react-native-elements";
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 
 const mapStateToProps = state => {
@@ -52,16 +53,19 @@ if (this.props.partners.isLoading) {
 if (this.props.partners.errMess) {
     return (
         <ScrollView>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
             <Mission />
             <Card
                 title='Community Partners'>
                 <Text>{this.props.partners.errMess}</Text>
             </Card>
+            </Animatable.View>
         </ScrollView>
     );
 }
 return (
     <ScrollView>
+     <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
     <Mission/>
     <Card title="Community Partners">
     <FlatList
@@ -70,6 +74,7 @@ return (
         keyExtractor = {item => item.id.toString()}
         />
     </Card>
+    </Animatable.View>
     </ScrollView>
 );
 
--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { ScrollView, Text } from "react-native";
 import { Card } from "react-native-elements";
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     constructor(props) {
@@ -14,6 +15,7 @@ class Contact extends Component {
     render() {
         return (
           <ScrollView>
+             <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
               <Card title="Contact Information" wrapperStyle={{ margin: 20 }}>
                 <Text>1 Nucamp Way</Text>
                 <Text>Seattle, WA 98001</Text>
@@ -21,6 +23,7 @@ class Contact extends Component {
                 <Text>Phone: 1-206-555-1234</Text>
                 <Text>Email: campsites@nucamp.co</Text>
               </Card>
+              </Animatable.View>
           </ScrollView>
         );
       }
