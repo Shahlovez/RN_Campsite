2ae8a83a0caafb0f2d2c1c76820e9386ed7d38b5
Shahlo Ovezniyazova <ovezniyazova@gmail.com>
2022-03-15 22:09:35 -0400

    ImageManipulator

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -6,6 +6,7 @@ import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
+import * as ImageManipulator from 'expo-image-manipulator';
 
 
 class LoginTab extends Component {
@@ -162,6 +163,30 @@ class RegisterTab extends Component {
             }
         }
     }
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(
+            imgUri,
+            [{ resize: {width: 400} }],
+            { format: ImageManipulator.SaveFormat.PNG }
+        );
+        console.log(processedImage);
+        this.setState({ imageUrl: processedImage.uri });
+    }
+
+    // getImageFromGallery = async () => {
+    //     const cameraRollPermission = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+    //     if (cameraRollPermission.status === 'granted') {
+    //         const capturedImage = await ImagePicker.launchImageLibraryAsync({
+    //           allowsEditing: true,
+    //           aspect: [1, 1]
+    //         });
+    //         if (!capturedImage.cancelled) {
+    //           this.processImage(capturedImage.uri)
+    //           console.log(capturedImage);
+    //         }
+    //     }
+    // }
 
     handleRegister() {
         console.log(JSON.stringify(this.state));
